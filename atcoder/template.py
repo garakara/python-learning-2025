1a10bbd74a2a2a3344e60a207e39f707c96dbffb
garakara <latexqp@hotmail.com>
2025-11-27 23:25:14 +0900

    first commit

---
+++ atcoder/template.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque, defaultdict, Counter
+from itertools import permutations, combinations
+import math
+
+def solve():
+    # 入力高速化
+    input = sys.stdin.readline
+    
+    # ここに解答コードを書く
+    n = int(input())
+    print(n)
+
+if __name__ == "__main__":
+    solve()
